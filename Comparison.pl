7351225f80311ee9a5b936f2d7e4faab42514938
filettispaghetti <rebeccafiletti@YO-NO-BRO.local>
2016-07-18 14:45:24 -0400

    Added my own TFIDF methods; added log10 method to use with IDF for proper log calculations; added a stopWords method to prevent repetition.

--- Comparison.pl
+++ Comparison.pl
@@ -1,19 +1,23 @@
-use strict;
+# use strict;
 use warnings 'all';
 
 use Text::Document;
 use Text::DocumentCollection;
 use fileOptions;
 use lib '/Users/rebeccafiletti/lib/';
-use Text::TFIDF;
 
 my $file = shift @ARGV;
 my $file2 = shift @ARGV;
 # my $file3 = shift @ARGV;
 
-# my $word = "Macbeth";
-# my $Obj = new Text::TFIDF(file=>[$file,$file2]);
-# print $Obj->TFIDF($file,$word);
+my $word = "Texas";
+
+print (myTF($file, $word));
+print "\n";
+print (myIDF($file, $file2, $word));
+print "\n";
+print (myTFIDF($file, $file2, $word));
+print "\n"; 
 
 # printFile($newfile);
 # printFile($newfile2);
@@ -21,9 +25,9 @@ my $file2 = shift @ARGV;
 # print newText($file);
 # print newText($file2);
 
-print "Cosine Similarity is: "; 
-print cosineSim($file, $file2);
-print "\n";
+# print "Cosine Similarity is: "; 
+# print cosineSim($file, $file2);
+# print "\n";
 
 # print "Weighted Cosine Similarity is: \n";
 # print WeightedCS($file, $file2, $file3);
--- fileOptions.pm
+++ fileOptions.pm
@@ -1,12 +1,12 @@
 package fileOptions;
 
-use strict;
+# use strict;
 use warnings;
 use Text::Document;
 use Getopt::Long;
 
 use Exporter qw(import);
-our @EXPORT = qw(printFile countFreq cosineSim WeightedCS);
+our @EXPORT = qw(printFile countFreq cosineSim WeightedCS myTF myIDF myTFIDF log10);
 
 sub printFile{
 	my ($file) = @_;
@@ -22,19 +22,7 @@ sub newText{
 	my ($file) = @_;
 	my %count1;
 	
-	# Assign Stop Words
-	my %sp;
-	my $wordlist = 'StopWords.txt';
-	my $lin;
-
-	open my $sw, '<', $wordlist or die "Could not open file. $!";
-		while ($lin = <$sw>) {
-    		chomp $lin;
-    	foreach my $str (split /\s+/, $lin) {
-       		$sp{lc($str)} = 1;
-        #print "$sp \n ";
-    	}
-		}
+	my %sp = stopWords();
 		
  	my $t1 = Text::Document->new();
  	
@@ -49,7 +37,7 @@ sub newText{
 		}}}
 		return $t1;
 	}
-	
+
 sub cosineSim{
 	my($file, $file2) =@_;
 	my $t1 = newText($file);
@@ -86,20 +74,89 @@ sub WeightedCS{
     print "\n";
 }
 }
-  
 
-# sub countLines{
-# 	my($file) = @_;
-# 	open(FILE, "<file.txt") or die "Could not open file: $!";
-# 
-# 	my ($lines, $words, $chars) = (0,0,0);
-# 
-# 	while (<FILE>) {
-#   		$lines++;
-#    		$chars += length($_);
-#     	$words += scalar(split(/\W+/, $_));
-# 	}
-# 
-# 	print("lines=$lines words=$words chars=$chars\n");
-#   }
-  
+sub myTF{
+	my ($file, $word) =@_;
+	my $count1;
+	my $count2;
+
+	my %sp = stopWords();
+	
+	open my $fh, '<', $file or die "Could not open file. $!";
+		while (my $line = <$fh>) {
+    		chomp $line;
+    		$count2 ++;
+    		$line =~ s/[[:punct:]]//g;
+   		foreach my $str (split /\s+/, $line) {
+    		if (!defined $sp{lc($str)}) {
+    			# print "$str \n";
+    			if (defined $word){
+    				$count1 ++;
+    				}
+    				}
+    				}
+    				}
+    				# print "$count1 \n";
+#     				print "$count2 \n";
+					return $count2/$count1;	
+	}
+	
+sub myIDF{
+	my ($file, $file2, $word) =@_;
+	my $count1 = 0;
+
+	my %sp = stopWords();
+	
+	open my $fh, '<', $file or die "Could not open file. $!";
+		while (my $line = <$fh>) {
+    	chomp $line;
+    	$line =~ s/[[:punct:]]//g;
+   		foreach my $str (split /\s+/, $line) {
+    	if (!defined $sp{lc($str)}) {
+    			if ($str eq $word){
+    				$count1 ++;
+    				last;
+    				}}}}
+    			
+    open my $fh2, '<', $file2 or die "Could not open file. $!";
+		
+		while (my $line2 = <$fh2>) {
+    		chomp $line2;
+    		$line2 =~ s/[[:punct:]]//g;
+   		foreach my $str2 (split /\s+/, $line2) {
+    		if (!defined $sp{lc($str2)}) {
+    			if ($str2 eq $word){
+    				$count1 ++;
+    				last;
+    				}}}}
+    				
+    		my $x = (log10(2))/$count1;		
+	return ($x);
+}
+
+sub myTFIDF{
+	my ($file, $file2, $word) =@_;
+	my $x = myTF($file, $file2);
+	my $y = myIDF($file, $file2, $word);
+	return $x * $y;
+}
+
+sub log10 {
+  my $n = shift;
+  return log($n)/log(10);
+}
+
+sub stopWords{
+	my %sp;
+	my $wordlist = 'StopWords.txt';
+	my $lin;
+
+	open my $sw, '<', $wordlist or die "Could not open file. $!";
+		while ($lin = <$sw>) {
+    		chomp $lin;
+    	foreach my $str (split /\s+/, $lin) {
+       		$sp{lc($str)} = 1;
+    	}
+		}
+	return %sp;
+}
