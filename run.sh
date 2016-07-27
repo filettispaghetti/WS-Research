6b50621a06fbadf663618b509184d71339c8b350
JenniferBenedict <jbenedict1@drew.edu>
2016-07-27 14:42:18 -0400

    edited run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,8 @@
 corpora="Documents Documents_raked"
 # files=`ls Documents`
 queries=`ls test-queries`
-mkdir -p querycorpusscores
+#mkdir -p querycorpusscores
 for c in corpora; do
-	for doc in c; do
-		for q in $queries; do
 # 		#echo Corpus10/$f
- 			perl query_corpus.pl q doc  querycorpusscores/$c$doc
- 		done
- 	done
+ 			perl query_corpus.pl queries c testout $c$doc
 done
