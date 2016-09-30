dca79180f56a363349c2fc8d168f887438c85102
JenniferBenedict <jbenedict1@drew.edu>
2016-09-30 15:06:10 -0400

    Removed query from goldset

--- tryy.sh
+++ tryy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash # optional — to run, bash rake_passages.sh
-files=`ls Documents`
-mkdir -p Documents_raked
+#files=`ls Documents`
+files=`ls Passages`
+#mkdir -p Documents_raked
+mkdir -p Passages_raked
 
 for f in $files; do
-	echo Documents/$f
-	echo Documents/$f | python rake.py > Documents_raked/$f
+	#echo Documents/$f
+	#echo Documents/$f | python rake.py > Documents_raked/$f
+	echo Passages/$f
+	echo Passages/$f | python rake.py > Passages_raked/$f
 done
